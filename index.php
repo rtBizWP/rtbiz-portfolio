fee231012f947aa370484c81c1dba079df9d23bc
Faishal Saiyed <saiyedfaishal@gmail.com>
2014-05-28 17:10:04 +0530

    Portfoli connection added

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php
 /*
-  Plugin Name: rtBiz Portfolio and Testimonials
-  Plugin URI: http://rtcamp.com/
-  Description: This plugin add Portfolio and Testimonial support
-  Version: 0.1-beta
-  Author: rtcamp
-  Author URI: http://rtcamp.com/
- */
+Plugin Name: rtBiz Portfolio
+Plugin URI: http://rtcamp.com/
+Description: This plugin add Portfolio support
+Version: 0.2-beta
+Author: rtcamp
+Author URI: http://rtcamp.com/
+*/
+
+define( 'RT_BIZ_PORTFOLIO', 'portfolio' );
+
+add_action( 'rt_biz_init', 'rt_biz_portfolio_init', 1 );
+
+function rt_biz_portfolio_init(){
+	$rt_biz_portfolio_loader = new RT_WP_Autoload( trailingslashit( dirname( __FILE__ ) ) . 'app' );
+
+	$rt_biz_init = new RT_Biz_Portfolio();
+}
